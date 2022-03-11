b1ea343f2ebda2ca2b93dec73837fe4cc41ceda1
Akshat <akshatchaudhary419@gmail.com>
2022-03-11 18:52:49 +0530

    sign_up_pages_completed

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -10,9 +10,75 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextField(
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child: Center(child: Text("Sign-In",
+                style: TextStyle(fontSize: 28),
+              )),
+            ),
+            SizedBox(height:100),
+            Container(
+              height: 60,
+              //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              width: MediaQuery.of(context).size.width*0.8,
+              decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: TextField(
+                      decoration:InputDecoration(hintText: '   E-Mail',
+                        border: InputBorder.none,
+                      ) ),
+                ),
+              ),
 
+            ),
+            SizedBox(height:35),
+
+            Container(
+              height: 60,
+              //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              width: MediaQuery.of(context).size.width*0.8,
+              decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: TextField(
+                      decoration:InputDecoration(hintText: '   Password',
+                        border: InputBorder.none,
+                      ) ),
+                ),
+              ),
+
+            ),
+            SizedBox(height:35),
+
+            ElevatedButton(onPressed: (){
+              //Navigator.push(
+                //context,
+                //MaterialPageRoute(builder: (context) => const SignIn()),
+            //  );
+            },
+              child: Text('Sign-In', style: TextStyle(fontSize: 26)),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                onPrimary: Colors.white,
+                shadowColor: Colors.greenAccent,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(200, 45), //////// HERE
+              ), //side: BorderSide(color: Colors.red)
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:game_space/pages/sign_in.dart';
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
 
@@ -9,11 +12,102 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Signup"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child: Center(child: Text("SignUp",
+              style: TextStyle(fontSize: 28),
+              )),
+            ),
+            SizedBox(height:100),
+            Container(
+              height: 60,
+              //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              width: MediaQuery.of(context).size.width*0.8,
+              decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: TextField(
+                      decoration:InputDecoration(hintText: '   E-Mail',
+                        border: InputBorder.none,
+                  ) ),
+                ),
+              ),
 
-      ],
+            ),
+            SizedBox(height:35),
+
+            Container(
+              height: 60,
+              //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              width: MediaQuery.of(context).size.width*0.8,
+              decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: TextField(
+                      decoration:InputDecoration(hintText: '   Password',
+                        border: InputBorder.none,
+                  ) ),
+                ),
+              ),
+
+            ),
+            SizedBox(height:35),
+
+            Container(
+              height: 60,
+              //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+              width: MediaQuery.of(context).size.width*0.8,
+              decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: TextField(
+                      decoration:InputDecoration(hintText: '  Confirm Password',
+                        border: InputBorder.none,
+                  ) ),
+                ),
+              ),
+            ),
+            SizedBox(height:40),
+            ElevatedButton(onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SignIn()),
+              );
+            }, child: Text('Register', style: TextStyle(fontSize: 26)),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              onPrimary: Colors.white,
+              shadowColor: Colors.greenAccent,
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0)),
+              minimumSize: Size(200, 45), //////// HERE
+            ), //side: BorderSide(color: Colors.red)
+            ),
+            SizedBox(height: 20),
+            RichText(text: TextSpan(
+    text: 'already have an account?',
+    style:  TextStyle(color: Colors.black87),
+    recognizer: TapGestureRecognizer()
+    ..onTap = () {Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SignIn()),
+    ); }
+            )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
