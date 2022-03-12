bdf1c0d75da2298555700b9b914ee37dd2d810ff
ayush <a_patidar@cs.iitr.ac.in>
2022-03-12 17:06:55 +0530

    andadu

--- lib/pages/profile_tab.dart
+++ lib/pages/profile_tab.dart
@@ -26,76 +26,34 @@ class _ProfileTabState extends State<ProfileTab> {
                 backgroundImage: NetworkImage('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
                 radius: 65,
               ),
-              SizedBox(height:60),
-              Container(
-                height: 60,
-                //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                width: MediaQuery.of(context).size.width*0.8,
-                decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Center(
-                    child: TextField(
-                        decoration:InputDecoration(hintText: '   Username',
-                          border: InputBorder.none,
-                        ) ),
-                  ),
+              SizedBox(height:10),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Text('Username',style: TextStyle(fontSize:16),),
                 ),
 
-              ),
-              SizedBox(height:35),
 
-              Container(
-                height: 60,
-                //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                width: MediaQuery.of(context).size.width*0.8,
-                decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Center(
-                    child: TextField(
-                        decoration:InputDecoration(hintText: '   Name',
-                          border: InputBorder.none,
-                        ) ),
-                  ),
-                ),
+              SizedBox(height:5),
 
-              ),
-              SizedBox(height:35),
-
-              Container(
-                height: 60,
-                //padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                width: MediaQuery.of(context).size.width*0.8,
-                decoration: BoxDecoration(color: Color(0xffebe9e9), borderRadius:BorderRadius.circular(400)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Center(
-                    child: TextField(
-                        decoration:InputDecoration(hintText: '  Enrollment no.',
-                          border: InputBorder.none,
-                        ) ),
-                  ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child:  Text('name',style: TextStyle(fontSize:14),),
                 ),
               ),
-              SizedBox(height:40),
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  MyHomePage(user: widget.user,)),
-                );
-              }, child: Text('Go', style: TextStyle(fontSize: 26)),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-                  onPrimary: Colors.white,
-                  shadowColor: Colors.greenAccent,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                  minimumSize: Size(200, 45), //////// HERE
-                ), //side: BorderSide(color: Colors.red)
+              SizedBox(height:70),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child:  Text('email-id',style: TextStyle(fontSize:22),),
+                ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height:30),
+              Container(height:40,width:MediaQuery.of(context).size.width,
+              decoration:BoxDecoration(color:Colors.grey),
+              )
+
             ],
           ),
         ),
